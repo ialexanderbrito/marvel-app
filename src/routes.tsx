d871050da79f97cbd5a7315ab3fbc14b5f0b38fa
Alexander <xanderzinho26@gmail.com>
2020-10-09 12:37:25 -0300

    :dizzy: Adicionado animação de navegação

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 
 import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 const AppStack = createStackNavigator();
 
 const Routes = () => {
   return (
     <NavigationContainer>
-      <AppStack.Navigator headerMode="none" screenOptions={{
-        cardStyle: {
-          backgroundColor: '#f0f0f5'
-        }
-      }} >
+      <AppStack.Navigator
+        headerMode="none"
+        screenOptions={{
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          headerShown: false,
+          cardStyle: {
+            backgroundColor: '#f0f0f5',
+          },
+        }}
+      >
         <AppStack.Screen name="Home" component={Home} />
-
+        <AppStack.Screen name="Detail" component={Detail} />
       </AppStack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default Routes;
