fbc68cf6f1fa0777d2ece4e00acdd05bd928ff20
Alexander <xanderzinho26@gmail.com>
2020-10-09 12:01:54 -0300

    :sparkles: Criado componente de Cards

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { API_URL } from '../../../.env.json';
+
+import { Container, Img, AlterEgo, Name } from './styles';
+
+interface ICharacter {
+  name: string;
+  alterEgo: string;
+  imagePath: string;
+}
+
+const Card: React.FC<ICharacter> = ({ name, alterEgo, imagePath }) => {
+  return (
+    <Container>
+      <Img source={{ uri: `${API_URL}/${imagePath}`}} />
+        <AlterEgo>{alterEgo}</AlterEgo>
+        <Name>{name}</Name>
+    </Container>
+  );
+};
+
+export default Card;
