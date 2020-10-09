9328a15809b9b957f662a4510da7dd5201ca9846
Alexander <xanderzinho26@gmail.com>
2020-10-09 19:45:31 -0300

    :sparkles: Criado componente SkillBar

---
+++ src/components/SkillBar/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const ProgressContent = styled.View`
+  width: 78%;
+  flex-direction: row;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+
+`;
+
+export const ProgressStrong = styled.View`
+  width: 2px;
+  height: 20px;
+  background-color: #FFF;
+
+`;
+
+export const ProgressWeak = styled.View``;
+
