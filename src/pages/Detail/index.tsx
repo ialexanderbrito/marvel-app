b35754106882ae827ff6cd6c0a42a08dad3ca1df
Alexander <xanderzinho26@gmail.com>
2020-10-09 13:56:20 -0300

    :sparkles: Adicionado info dos personagens e estilização

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,13 +1,117 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
-import { Container } from './styles';
+import { API_URL } from '../../../.env.json';
+
+import {
+  Container,
+  ImgBackground,
+  Gradient,
+  ToBack,
+  HeroContainer,
+  AlterEgo,
+  Name,
+  Characteristics,
+  CharacteristicContent,
+  CharacteristicsText,
+  BiographyContainer,
+  BiographyText,
+} from './styles';
+
+import Back from '../../assets/icons/back.svg';
+import Age from '../../assets/icons/age.svg';
+import Weight from '../../assets/icons/weight.svg';
+import Height from '../../assets/icons/height.svg';
+import Universe from '../../assets/icons/universe.svg';
+
+interface IParams {
+  character: {
+    name: string;
+    alterEgo: string;
+    imagePath: string;
+    biography: string;
+    caracteristics: {
+      birth: number;
+      weight: {
+        value: number;
+        unity: string;
+      };
+      height: {
+        value: number;
+        unity: string;
+      };
+      universe: string;
+    };
+    abilities: {
+      force: number;
+      intelligence: number;
+      agility: number;
+      endurance: number;
+      velocity: number;
+    };
+    movies: string[];
+  };
+}
 
 const Detail: React.FC = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const routeParams = route.params as IParams;
+
+  const { character } = routeParams;
+
   return (
-    <Container>
-      <Text>Detail</Text>
-    </Container>
+    <>
+      <ImgBackground source={{ uri: `${API_URL}/${character.imagePath}` }}>
+        <Gradient colors={['transparent', '#000']}></Gradient>
+        <Container>
+          <ToBack onPress={() => navigation.goBack()}>
+            <Back />
+          </ToBack>
+
+          <HeroContainer>
+            <AlterEgo>{character.alterEgo}</AlterEgo>
+            <Name>{character.name}</Name>
+          </HeroContainer>
+
+          <Characteristics>
+            <CharacteristicContent>
+              <Age />
+              <CharacteristicsText>{`${
+                new Date().getFullYear() - character.caracteristics.birth
+              } anos`}</CharacteristicsText>
+            </CharacteristicContent>
+
+            <CharacteristicContent>
+              <Weight />
+              <CharacteristicsText>
+                {character.caracteristics.weight.value}
+                {character.caracteristics.weight.unity}
+              </CharacteristicsText>
+            </CharacteristicContent>
+
+            <CharacteristicContent>
+              <Height />
+              <CharacteristicsText>
+                {character.caracteristics.height.value}
+                {character.caracteristics.height.unity[0]}
+              </CharacteristicsText>
+            </CharacteristicContent>
+
+            <CharacteristicContent>
+              <Universe />
+              <CharacteristicsText>
+                {character.caracteristics.universe}
+              </CharacteristicsText>
+            </CharacteristicContent>
+          </Characteristics>
+
+          <BiographyContainer>
+            <BiographyText>{character.biography}</BiographyText>
+          </BiographyContainer>
+        </Container>
+      </ImgBackground>
+    </>
   );
 };
 
--- src/pages/Detail/styles.ts
+++ src/pages/Detail/styles.ts
@@ -1,5 +1,73 @@
 import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+import { LinearGradient } from 'expo-linear-gradient'
 
-export const Container = styled.View`
+export const Container = styled.ScrollView``;
 
+export const ImgBackground = styled.ImageBackground`
+  flex: 1;
+  padding-horizontal: 32px;
+  padding-top: ${Constants.statusBarHeight + 20}px;
+`;
+
+export const Gradient = styled(LinearGradient)`
+  height: 150%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+`;
+
+export const ToBack = styled.TouchableOpacity``;
+
+export const HeroContainer = styled.View`
+  margin-top: 252px;
+`;
+
+export const AlterEgo = styled.Text`
+  font-family: 'Gilroy_500Medium';
+  font-size: 16px;
+  color: #FFF;
+  opacity: 0.75;
+  left: 14px;
+`;
+
+export const Name = styled.Text`
+  font-family: 'Gilroy_800Heavy';
+  font-size: 40px;
+  color: #FFF;
+  flex-shrink: 1;
+  left: 14px;
+  width: 180px;
+`;
+
+export const Characteristics = styled.View`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const CharacteristicContent = styled.View`
+  width: 60px;
+  height: 48px;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const CharacteristicsText = styled.Text`
+  font-family: 'Gilroy_500Medium';
+  font-size: 12px;
+  color: #FFF;
+  opacity: 0.75;
+`;
+
+export const BiographyContainer = styled.View``;
+
+export const BiographyText = styled.Text`
+  font-family: 'Gilroy_500Medium';
+  font-size: 14px;
+  color: #FFF;
+  opacity: 0.75;
 `;
