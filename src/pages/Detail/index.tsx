31ef8ac4f076edf51fa9731332591ac507e5123c
Alexander <xanderzinho26@gmail.com>
2020-10-09 12:38:31 -0300

    :sparkles: Iniciado página de Detail

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Detail: React.FC = () => {
+  return (
+    <Container>
+      <Text>Detail</Text>
+    </Container>
+  );
+};
+
+export default Detail;
